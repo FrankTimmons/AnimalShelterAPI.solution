72530a8e54e3916f63efaa162eec506966a308e0
FrankTimmons <franktimmonspdx@gmail.com>
2022-08-19 11:37:26 -0700

    Create Dog class blueprint.

---
+++ AnimalShelter/Models/Dog.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalShelter.Models
+{
+  public class Dog
+  {
+    public int DogId { get; set; }
+    public string Name { get; set; }
+    public string Breed { get; set; }
+    public string Gender { get; set; }
+  }
+}
