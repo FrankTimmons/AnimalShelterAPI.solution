fa4ce2337580604c99d59dcde427e6cd33bc5662
FrankTimmons <franktimmonspdx@gmail.com>
2022-08-19 15:59:45 -0700

    Seed data for the database.

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -12,7 +12,20 @@ namespace AnimalShelter.Models
     {
 
     }
-
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Dog>()
+          .HasData(
+              new Dog { DogId = 1, Name = "Marvin", Breed = "Labradoodle", Gender = "Male" },
+              new Dog { DogId = 2, Name = "Reggie", Breed = "Boston Terrier", Gender = "Male" },
+              new Dog { DogId = 3, Name = "Sushi", Breed = "Rat Terrier", Gender = "Female" },
+              new Dog { DogId = 4, Name = "Eva", Breed = "Greyhound", Gender = "Female" }
+          );
+      builder.Entity<Cat>()
+          .HasData(
+              new Cat { CatId = 1, Name = "Cuddles", Breed = "Tuxedo", Gender = "Female" }
+          );
+    }
     public DbSet<Dog> Dogs { get; set; }
     public DbSet<Cat> Cats { get; set; }
   }
