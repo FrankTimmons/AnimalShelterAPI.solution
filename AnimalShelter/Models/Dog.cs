58289196cfa9084cc1f0bdbbfae1f50c843c996a
FrankTimmons <franktimmonspdx@gmail.com>
2022-08-19 12:03:14 -0700

    Require fields on dog class.

--- AnimalShelter/Models/Dog.cs
+++ AnimalShelter/Models/Dog.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AnimalShelter.Models
 {
   public class Dog
   {
     public int DogId { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string Breed { get; set; }
+    [Required]
     public string Gender { get; set; }
   }
 }
