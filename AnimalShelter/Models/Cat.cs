d5511825f83c4103be2d10c4a8c7ac14a28dd19c
FrankTimmons <franktimmonspdx@gmail.com>
2022-08-19 12:04:16 -0700

    Create cat class.

---
+++ AnimalShelter/Models/Cat.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalShelter.Models
+{
+    public class Cat
+    {
+    public int CatId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Breed { get; set; }
+    [Required]
+    public string Gender { get; set; }
+    }
+}
